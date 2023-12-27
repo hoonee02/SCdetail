c959b21544fbe60603631961fdb510fea2496f45
hoonee02 <hoonee02@naver.com>
2023-12-27 20:21:26 +0900

    dcpy_Setting up Logging

--- discordbot.py
+++ discordbot.py
@@ -5,6 +5,9 @@ import os
 #디스코드패키지
 import discord
 
+#로깅 툴
+import logging
+import logging.handlers
 
 
 class MyClient(discord.Client):
@@ -19,8 +22,35 @@ intents.message_content = True
 
 client = MyClient(intents=intents)
 
+## 로깅
+
+# handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+# discord.utils.setup_logging()
+# discord.utils.setup_logging(level=logging.INFO, root=False)
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+logging.getLogger('discord.http').setLevel(logging.INFO)
+
+handler = logging.handlers.RotatingFileHandler(
+    filename='discord.log',
+    encoding='utf-8',
+    maxBytes= 32 * 1024 * 1024, #32MB
+    backupCount=5, #Rotate Through 5files
+)
+dt_fmt = '%Y-%m-%d %H:%M:%S'
+formatter = logging.Formatter('[{asctime}][{levelname:<8}] {name}:{message}', dt_fmt, style='{')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
 ## 디스코드 봇 토큰 가져오기
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client.run(TOKEN)
+client.run(
+    TOKEN, 
+    log_handler=None, 
+    # log_level=logging.DEBUG
+    root_logger=True,
+    )
